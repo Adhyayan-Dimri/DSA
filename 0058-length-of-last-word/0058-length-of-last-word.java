eaa51419dd95dbd267d1a945c785fb741157403e
Adhyayan Dimri <adhyayan02@icloud.com>
2026-07-30 17:56:36 +0530

    Time: 0 ms (100%), Space: 43.1 MB (52.35%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int l=0;
+        boolean started=false;
+        for(int i=s.length()-1;i>=0;i--){
+            
+            if(s.charAt(i)!=' '){
+                started=true;
+                l++;
+            }
+            else if (started){
+                break;
+            }
+
+            }
+        
+        return l;
+        
+    }
+}
